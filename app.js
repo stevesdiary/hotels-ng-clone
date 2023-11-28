9b74ba3151621f57fe7f42c15a47b006978340c2
steve-aella <soyeyemi@aellacredit.com>
2023-11-28 09:23:26 +0100

    Image Upload successful

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const  express = require ("express");
 const  Sequelize = require("sequelize");
+const cloudinary = require("cloudinary").v2;
 const mysql = require('mysql2');
+const fs = require('fs/promises');
+const fileUpload = require('express-fileupload');
 const cors = require ("cors");
 const app = express();
+const path = require("path");
+const multer = require("multer");
 const bodyParser = require('body-parser');
+// const { storage } = require("googleapis/build/src/apis/storage");
 const port = process.env.LOCAL_PORT || 3000 ;
-
 require('dotenv').config();
-
 const connection = mysql.createConnection({
    host: process.env.HOST,
    port: process.env.PORT,
@@ -15,21 +19,54 @@ const connection = mysql.createConnection({
    password: process.env.PASSWORD,
    database: process.env.DATABASE
 })
-
-// connection.connect((err) => {
-//    if (err)throw err;
-//    console.log('Connected to MYSQL server')
-// })
-
+cloudinary.config({
+   cloud_name: process.env.CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+   secure: true,
+});
+connection.connect((err) => {
+   if (err)throw err;
+   console.log('Connected to MYSQL server')
+})
+const options = {
+   use_filename: true,
+   unique_filename: false,
+   overwrite: false,
+};
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, './uploads');
+   },
+   filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));  
+   }
+});
 
+let upload = multer({
+   storage: storage,
+   // dest: './uploads/',
+   // limits: {fileSize: 1000000},
+})
 app.get('/', (req, res) => {
    res.send("Description.")
 })
-
-
-
+app.post('/upload', upload.single('image'), async(req, res) => {
+   try {
+      let imagePath = "./uploads/" + req.file.filename;
+      
+      console.log('Upload status', imagePath)
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      return res.status(200).send({message: "Upload Successful", result: result});
+   }
+   catch(err) {
+      console.log(err)
+   }
+});
 
 app.listen(port, async() => {
-   console.log('App running on port: '+ port)
+   console.log('App running on port: ' + port)
 })
--- file.upload.js
+++ file.upload.js
@@ -1,26 +1,36 @@
-const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
-const projectId = process.env.PROJECT_ID;
-const bucketName = process.env.BUCKET_NAME;
+// const cloudinary = require("cloudinary").v2;
+// require("dotenv").config();
+// const projectId = process.env.PROJECT_ID;
+// const bucketName = process.env.BUCKET_NAME;
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
-});
-const imagePath = "./screenshot.png";
-// Uploads an image file
-const options = {
-    use_filename: false,
-    unique_filename: true,
-    overwrite: true,
-};
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//     secure: true,
+// });
+// const imagePath = "./screenshot.png";
+// // Uploads an image file
+// const options = {
+//     use_filename: false,
+//     unique_filename: true,
+//     overwrite: true,
+// };
 
 
-const result = cloudinary.uploader.upload(imagePath, options).then(result => {
-    console.log(result.url);
-})
+// async function imageUpload(imagePath, options){
+//     try{
+//         const result = await cloudinary.uploader.upload(imagePath, options);
+//         console.log("Processing...")
+//         console.log("Upload successful", result.url);
+//         return (result);
+//     }
+//     catch(err){
+//         console.log('Upload failed, an error occoured!', err)
+//     }
+    
+// }
+// imageUpload(imagePath, options);
 
 
 
