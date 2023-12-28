62e3367efd3485a77435c7afbbfd1eaff9dfa677
stevesdiary <stephenoyeyemi@gmail.com>
2023-12-28 22:06:30 +0100

    Confirm password

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,19 +6,24 @@ const saltRounds = bcrypt.genSaltSync(11);
 const registerController = {
   registerUser: async (req, res) => {
     try{
-      const { first_name, last_name, phone_number, email, password, type } = req.body;
+      const { first_name, last_name, phone_number, email, password, confirm_password, type } = req.body;
       const id = uuidv4();
       console.log(email, "EMAIL", first_name);
       const userExists = await User.findOne({ where: {email} });
       
       if(userExists && userExists.length > 0){
-        return res.status(409).json({ message: `User ${first_name} already exists`});
+        return res.status(409).json({ message: `User ${first_name} already exists, you can login with your password.`});
       }
-      const hashedPassword = await bcrypt.hash(password, saltRounds)
+      if (password !== confirm_password) {
+        res.status(409).send({message: 'Password do not match, check and try again.'})
+      }
+      if(password == confirm_password) {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+      
       const user_record = await User.create({ id, first_name, last_name, phone_number, email, password: hashedPassword, type });
-      console.log("User created", user_record);
+      // console.log("User created", user_record);
       return res.status(201).send({ message: `User record for ${first_name} has been created successfully`, user_record })
-  
+      }
     }catch(err) {
       console.log(err)
       return res.status(500).send({message: "An error occoured!", err});
