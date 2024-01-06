2dc20afa0b299d5350bcef3c62f3c9f396b1309b
stevesdiary <stephenoyeyemi@gmail.com>
2024-01-06 23:11:56 +0100

    Added password reset route

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-
+const verifyType = require('../middleware/verifyType').default;
 const passwordResetController = require('../controllers/passwordResetController');
 
-router.put('/:token', passwordResetController.resetPassword);
+router.put('/:token', verifyType(['premium', 'regular', 'guest', 'admin']), passwordResetController.resetPassword);
 
 module.exports = router;
