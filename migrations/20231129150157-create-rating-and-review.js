08dcface7244ed629ee1df20ff80ece969900a20
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-29 12:54:59 +0100

    Update 20231129150157-create-rating-and-review.js

--- migrations/20231129150157-create-rating-and-review.js
+++ migrations/20231129150157-create-rating-and-review.js
@@ -51,6 +51,10 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
       }
     });
   },
