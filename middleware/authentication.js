3f89101c038b9293a13d283d9c9355d90a5e57f9
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-30 00:54:03 +0100

    Update authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,21 +3,20 @@ const secret = process.env.JWT_SECRET;
 
 
 const authentication = async (req, res, next) => {
-  const authHeader = req.headers['Authorization'];
-  let token = req.headers.authorization
-  if (!token) {
-      return res.send("Provide correct token first!")
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    console.log("No auth header here!");
+      return res.status(401).send("Provide correct token first!")
   }
-  token = authHeader && authHeader.split(' ')[1];
-  if(token == null || token !== token){ 
+  const token = authHeader.split(' ')[1];
+  if(token == null){ 
     return res.status(401).json({message: 'Unauthorized or wrong token!'})
   }
   try{
-    const decoded = jwt.verify(token, secret, (err, user));
-    console.log("DECODED USER DATA ", decoded);
-    if(decoded.UserInfo){
-      req.email = decoded.UserInfo.email;
-      req.type = decoded.UserInfo.type;
+    const decoded = jwt.verify(token, secret);
+    if(decoded){
+      req.email = decoded.email;
+      req.type = decoded.type;
       next();
     }
     else{
@@ -27,8 +26,9 @@ const authentication = async (req, res, next) => {
     }
   }
   catch(err){
-    return res.status(500).send({Message: 'An error occoured', Error: err})
+    console.log(err, err.message);
+    return res.status(500).send({Message: 'An error occoured', Error: err.message})
   }
 }
 
-module.exports = {authentication};
+module.exports = { authentication };
