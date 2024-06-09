b804272da1130a9701db4ed93654d1500c5c431a
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-06-09 22:21:35 +0100

    Update login.js
    Verify user type

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
 const { authentication } = require('../middleware/authentication');
+const verifyUserType = require('../middleware/verifyUserType');
 
 router.post('/login', loginController.login); //, authentication(['admin'])
 
