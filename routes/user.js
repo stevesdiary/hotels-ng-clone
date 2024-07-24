ebf75945aa2a6dec16893b54b8171f69dbee1fb5
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-07-24 10:14:10 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const verifyType = require('../middleware/verifyUserType');
 const userController = require('../controllers/usersController');
 const { authentication } = require('../middleware/authentication');
-const verifyUserType = require('../middleware/verifyUserType');
+const authorise = require('../middleware/verifyUserType');
 
-router.get('/alluser', authentication, verifyUserType('admin'), userController.findAllUser); //authentication, verifyType(['admin']),
+router.get('/alluser', authentication, authorise('admin', 'user'), userController.findAllUser);
 
-router.get('/user/:id', authentication, verifyUserType('admin'), userController.findOne); // authentication, verifyType(['admin']),
+router.get('/user/:id', authentication, authorise('admin', 'user'), userController.findOne);
 
-router.put('/updateuser/:id', authentication, verifyUserType('admin'), userController.updateUser); //authentication, verifyType(['admin'])
+router.put('/updateuser/:id', authentication, authorise('admin'), userController.updateUser);
 
-router.delete('/deleteuser/:id', authentication, verifyUserType('admin'), userController.deleteUser);
+router.delete('/deleteuser/:id', authentication, authorise('admin'), userController.deleteUser);
 
 module.exports = router;
